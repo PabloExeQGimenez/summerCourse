5bc01fe325558688a511535ad982658e39863d4b
Pablo Exequiel Gimenez <142545389+PabloExeQGimenez@users.noreply.github.com>
2024-01-03 14:39:42 -0300

    inicio

---
+++ src/test/java/com/mindhub/summercourse/SummercourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindhub.summercourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummercourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
